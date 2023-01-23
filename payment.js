edc9be0e5c56ff3a6b1164e256529bd21d542b6f
suraj <soorajthakur857@gmail.com>
2023-01-23 22:48:14 +0530

    final changes

--- payment.js
+++ payment.js
@@ -1,10 +1,29 @@
 let key=document.getElementById("submit");
+let arr = [1234,1254,2563,2589]
+let count = 0
+let x = 0
+let otp = document.getElementById("otp")
 
 key.addEventListener("click",()=>{
-    
-    alert("PLEASE ENTER OTP");
-    
-    window.location="happyshopping.html"
+    let value = otp.value
+   
 
+    
+    
+    if(value == arr[2]){
+        alert(`YOUR PAYMENT HAS BEEN SUCESSFUL`);
+        window.location = "./happyshopping.html"
+        window.location.assign(happyshopping.html)
+    }
+    else{
+        alert("PLEASE ENTER CORRECT OTP")
+    }
+    x++
+
+})
+
+document.getElementById("btn").addEventListener("click",()=>{
+    alert(`YOUR OTP IS ${arr[2]} `)
+    count++
 })
 
